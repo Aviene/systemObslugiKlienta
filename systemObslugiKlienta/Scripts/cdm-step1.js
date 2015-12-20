c6baca0da92ab6de39c2d88eece04c4421519f99
Aviene <aviene@o2.pl>
2015-12-20 18:00:11 +0100

    3 steps
    Client Database Management 3 Steps

---
+++ systemObslugiKlienta/Scripts/cdm-step1.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('div#step1 div.div-huge-btn a').on('click', function () {
+
+        $("div#step1 div.div-huge-btn a").removeClass("btn-primary");
+        $("div#step1 div.div-huge-btn a").addClass("huge-btn-default");
+        $("div#step1 div.div-huge-btn a span.glyphicon").removeClass("glyphicon-check");
+        $("div#step1 div.div-huge-btn a span.glyphicon").addClass("glyphicon-unchecked");
+
+        $(this).removeClass("huge-btn-default");
+        $(this).addClass("btn-primary");
+        $("span.glyphicon", this).removeClass("glyphicon-unchecked");
+        $("span.glyphicon", this).addClass("glyphicon-check");
+        $("div#step1 a.next-step").removeClass("disabled");
+        $("div#steps-nav ul.nav-tabs li#step2-tab").removeClass("disabled");
+    });
+})
---
+++ systemObslugiKlienta/Scripts/tabs.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('ul.nav-tabs li a').on('click', function (e) {
+        e.preventDefault();
+        if (!$(this).parent().hasClass('disabled')) {
+            if (!$(this).hasClass('blocked')) {
+                var href = $(this).attr('href');
+                console.log(href);
+                if (!$(this).parent().hasClass('active')) {
+                    $('ul.nav-tabs li a').addClass('blocked');
+                    $('ul.nav-tabs li').removeClass('active');
+                    $('#steps-content .tab').slideUp(600);
+                    $(this).parent().addClass('active');
+                    $(href).delay(600).slideDown(600,
+                    function () {
+                        $('ul.nav-tabs li a').removeClass('blocked');
+                    });
+                }
+            }
+        }
+    });
+})
